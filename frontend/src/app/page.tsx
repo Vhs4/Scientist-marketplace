50969596be555f8dc29ddc2a0d7b01a151517355
Vhs4 <contatovhs4@gmail.com>
2023-10-07 12:53:50 -0300

    [FEAT] Daisiuy add

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,23 @@
-
+import Link from "next/link"
 
 export default function Home() {
   return (
-   <h1></h1>
+    <header className="flex justify-between">
+  <nav>
+      <ul className="flex">
+        
+      <Link href={'#'}> Logo </Link>
+        <li className="bold ml-4 mr-4">imagem</li>
+        <li className="bold mr-4">teste</li>
+        <li className="bold mr-4">teste</li>
+        <li className="bold mr-4">teste</li>
+        <li className="bold">teste</li>
+      </ul>
+   </nav>
+   <ul>
+    <button className="mr-4">Eae</button>
+    <button className="mr-4">Eae</button>
+   </ul>
+   </header>
   )
 }
