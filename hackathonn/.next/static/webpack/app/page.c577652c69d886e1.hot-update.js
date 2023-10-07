78f2a93238fe06ed780566552dcaa58cf49deea6
Vhs4 <contatovhs4@gmail.com>
2023-10-07 11:11:31 -0300

    [FEAT] Import CSS

---
+++ hackathonn/.next/static/webpack/app/page.c577652c69d886e1.hot-update.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5CCarla%5COneDrive%5C%C3%81rea%20de%20Trabalho%5CScientist-marketplace%5Chackathonn%5Csrc%5Cstyles%5Cstyles.css&server=false!":
+/*!********************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5CCarla%5COneDrive%5C%C3%81rea%20de%20Trabalho%5CScientist-marketplace%5Chackathonn%5Csrc%5Cstyles%5Cstyles.css&server=false! ***!
+  \********************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./src/styles/styles.css */ \"(app-pages-browser)/./src/styles/styles.css\"))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz1DJTNBJTVDVXNlcnMlNUNDYXJsYSU1Q09uZURyaXZlJTVDJUMzJTgxcmVhJTIwZGUlMjBUcmFiYWxobyU1Q1NjaWVudGlzdC1tYXJrZXRwbGFjZSU1Q2hhY2thdGhvbm4lNUNzcmMlNUNzdHlsZXMlNUNzdHlsZXMuY3NzJnNlcnZlcj1mYWxzZSEiLCJtYXBwaW5ncyI6IkFBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz83NzY0Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiICovIFwiQzpcXFxcVXNlcnNcXFxcQ2FybGFcXFxcT25lRHJpdmVcXFxcw4FyZWEgZGUgVHJhYmFsaG9cXFxcU2NpZW50aXN0LW1hcmtldHBsYWNlXFxcXGhhY2thdGhvbm5cXFxcc3JjXFxcXHN0eWxlc1xcXFxzdHlsZXMuY3NzXCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5CCarla%5COneDrive%5C%C3%81rea%20de%20Trabalho%5CScientist-marketplace%5Chackathonn%5Csrc%5Cstyles%5Cstyles.css&server=false!\n"));
+
+/***/ }),
+
+/***/ "(app-pages-browser)/./src/styles/styles.css":
+/*!*******************************!*\
+  !*** ./src/styles/styles.css ***!
+  \*******************************/
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (\"ad57ec370cb2\");\nif (true) { module.hot.accept() }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL3NyYy9zdHlsZXMvc3R5bGVzLmNzcyIsIm1hcHBpbmdzIjoiO0FBQUEsK0RBQWUsY0FBYztBQUM3QixJQUFJLElBQVUsSUFBSSxpQkFBaUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vc3JjL3N0eWxlcy9zdHlsZXMuY3NzP2NiZWIiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgXCJhZDU3ZWMzNzBjYjJcIlxuaWYgKG1vZHVsZS5ob3QpIHsgbW9kdWxlLmhvdC5hY2NlcHQoKSB9XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./src/styles/styles.css\n"));
+
+/***/ })
+
+});
