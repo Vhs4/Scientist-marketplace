c495340383b6116fb399b1fdf92262293459ac80
Lucas <you@example.com>
2023-10-08 18:28:40 -0300

    BUG FIX: change .gitignore

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-engine = create_engine("sqlite:///base.db", echo=True)
+engine = create_engine("sqlite:///base.sqlite3", echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
